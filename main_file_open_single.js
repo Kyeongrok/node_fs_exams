fcdbf09696128bf28de9f4e2475aaa82c2dff907
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-16 13:39:20 +0900

    daum lineup page 파싱

--- main_file_open_single.js
+++ main_file_open_single.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
-const path = '.\\';
+const path = '.\\test_data\\basketball\\daum\\';
 const fileNameList = [
   'daum_json.json',
   'daum_kbl_game2.json',
   'daum_kbl_game3.json',
+  'kbl_game4.json',
+  'kbl_game5.json',
+  'kbl_game6.json',
 ]
 
 // console.log(json);
@@ -25,6 +28,13 @@ const printData = (filePathName) => homeName => awayName =>{
   });
 }
 
+// gameId = 8008133
+// daum lineup page: http://sports.media.daum.net/sports/gamecenter/80008133/highlight
+// daum api url: http://score.sports.media.daum.net/hermes/api/game/get.json?gameId=80008133&detail=liveData&dummy=pull_mode_started_150796992&callback=seulgiCallback
 printData(path + fileNameList[0])('안양')('삼성');
 printData(path + fileNameList[1])('고양')('창원');
 printData(path + fileNameList[2])('울산모비스')('부산KT');
+printData(path + fileNameList[3])('인천전자랜드')('안양KGC');
+printData(path + fileNameList[4])('원주DB')('전주KCC');
+printData(path + fileNameList[5])('서울SK')('고양오리온');
+
